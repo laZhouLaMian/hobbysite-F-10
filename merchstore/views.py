a0682194b76d90f1f4007e872bff97a36d1ce0d7
Vaughn Alvarez <vaughn.alvarez@obf.ateneo.edu>
2024-03-19 21:51:52 +0800

    created merchstore views

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,6 +1,14 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+from .models import ProductType, Product
+
 
 class ProductListView(ListView):
     model = Product
+    template_name = "merchstore_product_list.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "merchstore_product_detail.html"
