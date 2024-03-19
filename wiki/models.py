ae6a07f7559c97f09426292cb6957f53bbc9aad7
michael-gdc <michael.delcastillo@student.ateneo.edu>
2024-03-19 21:26:56 +0800

    added hobbysite wiki app and templates

---
+++ wiki/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        on_delete=models.SET_NULL,
+        related_name="articles",
+        default=1,
+        null=True,
+    )
+    entry = models.TextField()
+
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse("wiki:article-detail", args=[self.pk])
+
+    class Meta:
+        ordering = ["-created_on"]
