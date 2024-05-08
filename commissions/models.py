a7b0ff51a427374d8a8c059859aa790a51a748db
laZhouLaMian <joseph.duron@student.ateneo.edu>
2024-05-08 15:14:35 +0800

    add Job Model

--- commissions/models.py
+++ commissions/models.py
@@ -34,13 +34,24 @@ class Commission(models.Model):
         return reverse("commissions:detail", args=[str(self.pk)])
 
 
-class Comment(models.Model):
+class Job(models.Model):
+    STATUS_CHOICES = {
+        OPEN: "Open",
+        FULL: "Full",
+    }
+        
     commission = models.ForeignKey(
-        Commission, on_delete=models.CASCADE, related_name="comments"
+        Commission, on_delete=models.CASCADE, related_name="job"
     )
-    entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default=STATUS_CHOICES[OPEN], choices=STATUS_CHOICES)
+    manpower_required = models.IntegerField()
+
+    class Meta:
+        ordering = ["status", "-manpower_required", "role"]
+
+    def __str__(self):
+        return str(self.role)
 
     class Meta:
         ordering = ["-created_on"]
